7c5c35c4fdf2d8aff76540c5e504a96139fdb17e
oibene <alvespbene@gmail.com>
2022-06-18 20:20:55 -0300

    teste?

---
+++ src/Inter2022/atvB.java
@@ -0,0 +1,64 @@
+package Inter2022;
+
+import java.util.Scanner;
+
+public class atvB {
+	 public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 
+		// armazena linha
+		 System.out.print("");
+		 String texto = sc.nextLine();
+		 
+		 // separa primeira linha em 2 var e converte
+		 String linha[] = texto.split(" ");
+		 int P = Integer.parseInt(linha[0]);
+		 int E = Integer.parseInt(linha[1]);
+		 
+		 // matriz contato e lista inseguro
+		 
+		 
+		 // contadores comandC e contact
+		 int comandC = 0, contact = 0;
+		 
+		 // loop com E entradas
+		 
+		 do {
+			// armazena cada linha
+			System.out.print("");
+			texto = sc.nextLine();
+			String linhas[] = texto.split(" ");
+			 
+			String C = linhas[0];
+			// comandos
+			switch(C) {
+				case "c":
+					comandC++;
+				break;
+				
+				case "p":
+					comandC++;
+				break;
+				
+				case "n":
+					comandC++;
+				break;
+				
+				case "ns":
+					comandC++;
+				break;
+				
+				case "ni":
+					comandC++;
+				break;
+				
+				case "ii":
+					comandC++;
+				break;
+			}
+			
+			E--;
+		 }while (E != 0);
+		 
+	}
+}
